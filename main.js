6929ff2684b7b0dfe9e3911896df61505f970dbb
JoelJeffrey <JoelJeffrey.dev@gmail.com>
2013-12-21 17:20:25 -0800

    finally!

--- main.js
+++ main.js
@@ -1,6 +1,10 @@
 $('.panel').on('click', function() {
 	console.log('working?');
-	$this = $(this);
-	$('.center').empty('p');
-	$this.find('p').appendTo('.center');
-	});
+	var $this = $(this);
+	var msg = $this.find('p').html();
+	var newMsg = $('<p>' + msg + '</p>');
+
+    $('.panel').removeClass('is-active');
+    $this.addClass('is-active');
+    $('.center').empty().append(newMsg);
+});
