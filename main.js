9edd7bc1ef4c791d2d46c33fd8a6908084d3cab8
JoelJeffrey <JoelJeffrey.dev@gmail.com>
2013-12-22 01:59:51 -0800

    this should work wonderfully!

--- main.js
+++ main.js
@@ -1,10 +1,17 @@
+$(document).ready(function() {
+	skrollr.init();
+});
+
 $('.panel').on('click', function() {
 	console.log('working?');
 	var $this = $(this);
 	var msg = $this.find('p').html();
 	var newMsg = $('<p>' + msg + '</p>');
 
-    $('.panel').removeClass('is-active');
-    $this.addClass('is-active');
-    $('.center').empty().append(newMsg);
+	if($this.hasClass('is-active')) {
+		return false;
+	}
+    	$('.panel').removeClass('is-active');
+    	$this.addClass('is-active');
+    	$('.center').empty().append(newMsg);
 });
